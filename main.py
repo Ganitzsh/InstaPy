837adf72778f44859bd9dbb7dd3ec39402ae91c8
Julien Ganichot <julien.ganichot@gmail.com>
2020-02-12 17:41:13 +0100

    Remove everythom

--- main.py
+++ main.py
@@ -25,10 +25,13 @@ except Exception as e:
     # Settings.database_location = '/path/to/instapy.db'
 
 
-session = InstaPy(username=insta_username,
-                  password=insta_password,
-                  headless_browser=headless,
-                  multi_logs=True)
+session = InstaPy(
+        username=insta_username,
+        password=insta_password,
+        headless_browser=headless,
+        want_check_browser=False,
+        multi_logs=True
+)
 
 try:
     with open(sys.argv[1], encoding='utf-8') as f:
@@ -41,34 +44,54 @@ try:
         min_follower = data['MinFollowers']
         max_following = data['MaxFollowing']
         min_following = data['MinFollowing']
-        potency = data['Potency']
         per_user = data['PerUser']
     pass
+
     session.login()
 
-    # settings
-    # selection = sample(hashtags, s) # Select sample random hashtags
-    # session.set_comments(comments)
-    # session.set_do_comment(True, percentage=50)
-    # session.set_smart_hashtags(selection, limit=3, sort='top', log_tags=True)
-    ratio = -1.0
-    # if potency == 'positive':
-    #     ratio = max_follower / max_following
-    # elif potency == 'negative':
-    #     ratio = -(max_following / max_follower)
+    session.set_quota_supervisor(
+            enabled=True, 
+            sleep_after=["likes", "comments_d", "follows", "unfollows", "server_calls_h"],
+            sleepyhead=True,
+            stochastic_flow=True,
+            notify_me=True,
+            peak_likes_hourly=57,
+            peak_likes_daily=585,
+            peak_comments_hourly=21,
+            peak_comments_daily=182,
+            peak_follows_hourly=48,
+            peak_follows_daily=None,
+            peak_unfollows_hourly=35,
+            peak_unfollows_daily=402,
+            peak_server_calls_hourly=None,
+            peak_server_calls_daily=4700
+    )
 
-    session.set_user_interact(amount=per_user, randomize=True, percentage=50, media='Photo')
+    session.set_skip_users(
+            skip_private=True,
+            skip_no_profile_pic=True,
+            no_profile_pic_percentage=100
+    )
+
+    session.set_user_interact(
+            amount=per_user,
+            randomize=True,
+            percentage=100,
+            media='Photo'
+    )
+
     session.set_relationship_bounds(
             enabled=True,
-            potency_ratio=ratio,
             delimit_by_numbers=True,
+            min_posts=min_posts,
+            max_posts=max_posts,
             max_followers=max_follower,
             min_followers=min_follower,
             max_following=max_following,
             min_following=min_following,
     )
-    session.like_by_tags(hashtags, use_smart_hashtags=False, randomize=True)
-    session.like_by_feed(amount=total_likes, randomize=True, unfollow=False, interact=True)
+
+    session.like_by_tags(hashtags, randomize=True)
 
 except Exception as exc:
     # if changes to IG layout, upload the file to help us locate the change
